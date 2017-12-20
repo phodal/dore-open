fb97afe2c703d0fee7da69cd36e7d573de533f16
Phodal HUANG <h@phodal.com>
2017-12-20 23:34:01 +0800

    [T] add ios open code

--- ios/RCTDoreOpen/RCTDoreOpen.h
+++ ios/RCTDoreOpen/RCTDoreOpen.h
@@ -1,5 +1,11 @@
-#import "RCTBridge.h"
+#import <React/RCTBridgeModule.h>
+#import <Foundation/Foundation.h>
+#import <QuickLook/QuickLook.h>
 
-@interface DoreOpen : NSObject <RCTBridgeModule>
+@interface DoreOpen : NSObject <QLPreviewControllerDelegate,QLPreviewControllerDataSource, QLPreviewItem>
+
+@property(strong, nonatomic) NSURL *fileUrl;
+@property(readonly) NSURL *previewItemURL;
+@property (nonatomic, copy) NSString* callbackId;
 
 @end
