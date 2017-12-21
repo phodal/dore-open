a76342a602f68862386a42089a18722cf93c51cc
Phodal HUANG <h@phodal.com>
2017-12-21 19:57:04 +0800

    [T] remove unused files

--- index.android.js
+++
@@ -1,9 +0,0 @@
-import React from 'react-native';
-
-const DoreOpen = React.NativeModules.DoreOpen;
-
-export default {
-  open: (onSuccess, onFailure) => {
-    return DoreOpen.open(onSuccess, onFailure);
-  },
-};
--- index.ios.js
+++
@@ -1,9 +0,0 @@
-import React from 'react-native';
-
-const DoreOpen = React.NativeModules.DoreOpen;
-
-export default {
-  open: () => {
-    return DoreOpen.open();
-  },
-};
