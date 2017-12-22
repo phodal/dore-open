27c4e46ccde1805fed2156f500ff204045d6cbe2
Phodal HUANG <h@phodal.com>
2017-12-22 20:18:57 +0800

    [T] update code

--- iOS/DoreOpen/DoreOpen.m
+++ iOS/DoreOpen/DoreOpen.m
@@ -7,6 +7,7 @@
 
 @property UIView* previewView;
 @property QLPreviewController* previewCtrl;
+@property NSString *_url;
 
 @end
 
@@ -19,7 +20,7 @@ RCT_EXPORT_MODULE()
 RCT_EXPORT_METHOD(open:(NSString *) path) {
     if (path != nil && [path length] > 0) {
         NSURL *url = [NSURL URLWithString:path];
-        self.fileUrl = url;
+        self._url = url;
         
         self.previewCtrl = [[QLPreviewController alloc] init];
         self.previewCtrl.delegate = self;
@@ -41,15 +42,17 @@ RCT_EXPORT_METHOD(open:(NSString *) path) {
             height= window.frame.size.height;
         }
         self.previewCtrl.view.frame = CGRectMake(0, 0, width, height);
+        [self.previewCtrl refreshCurrentPreviewItem];
 
         dispatch_async(dispatch_get_main_queue(), ^{
+
             [window addSubview:self.previewCtrl.view];
             
             [window makeKeyAndVisible];
 
         });
     } else {
-        NSLog(@"cordova.disusered.open - Missing URL argument");
+        NSLog(@"Dore.open - Missing URL argument");
     }
 }
 
@@ -68,7 +71,7 @@ RCT_EXPORT_METHOD(open:(NSString *) path) {
 #pragma mark - QLPreviewControllerDelegate
 
 - (BOOL)previewController:(QLPreviewController *)controller shouldOpenURL:(NSURL *)url forPreviewItem:(id <QLPreviewItem>)item {
-    return YES;
+    return [NSURL URLWithString:self._url];
 }
 
 - (void)previewControllerDidDismiss:(QLPreviewController *)controller {
@@ -76,7 +79,7 @@ RCT_EXPORT_METHOD(open:(NSString *) path) {
 }
 
 - (NSURL *)previewItemURL {
-    return self.fileUrl;
+    return self._url;
 }
 
 @end
